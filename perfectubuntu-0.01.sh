31d7db662912d308dfc0f9c98767c61f36a06905
jfreitas2015 <jeanrfreitass@gmail.com>
2015-04-01 16:45:59 -0300

    Perfect Ubuntu Script Beta 0.01
    Perfect Ubuntu Script Beta 0.01

---
+++ perfectubuntu-0.01.sh
@@ -0,0 +1,109 @@
+#Adding Applications PPA's
+echo Adding Aplications PPAs ...
+sudo sh -c ‘echo “deb http://archive.canonical.com/ quantal partner” >> /etc/apt/sources.list’
+sudo add-apt-repository ppa:atareao/atareao -y
+sudo add-apt-repository ppa:atareao/atareao -y
+sudo add-apt-repository ppa:linrunner/tlp -y
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+sudo add-apt-repository ppa:foobnix-player/foobnix -y
+sudo add-apt-repository ppa:ricotz/docky -y
+sudo add-apt-repository ppa:audacity-team/daily -y
+sudo add-apt-repository ppa:jonoomph/openshot-edge -y
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+echo Adding Aplications PPAs [OK]
+
+#Updating and upgrading packages
+sudo apt-get update && sudo apt-get upgrade -y
+
+#Deja-dup backup tool
+sudo apt-get install deja-dup -y
+
+#Terminal Notifications
+sudo apt-get install undistract-me -y
+
+#Y PPA Manager
+sudo apt-get install y-ppa-manager -y
+
+#Vuze torrent client
+sudo apt-get install vuze -y
+
+#Google Chrome
+sudo apt-get install google-chrome-stable -y
+
+#Adobe Flash plugin
+sudo apt-get install flashplugin-installer -y
+
+#VLC Player
+sudo apt-get install vlc -y
+
+#Openshot Video Editor
+sudo apt-get install openshot openshot-doc -y
+
+#Install some Codecs and Enable DVD Playback
+sudo apt-get install gstreamer0.10-plugins-ugly libxine1-ffmpeg gxine mencoder libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321 libavcodec-extra -y
+sudo /usr/share/doc/libdvdread4/install-css.sh
+
+#Foobnix Audio Player
+sudo apt-get update && sudo apt-get install foobnix -y
+
+#Spotify
+sudo apt-get install spotify-client -y
+
+#Audacity
+sudo apt-get install audacity -y
+
+#Plank Dock
+sudo apt-get install plank -y
+
+#RAR
+sudo apt-get install rar -y
+
+#GIMP
+sudo apt-get install gimp -y
+
+#Unity Tweak Tool
+sudo apt-get install unity-tweak-tool -y
+
+#Pidgin
+sudo apt-get install pidgin -y
+
+#Skype
+sudo apt-get install skype -y
+
+#Java
+sudo apt-get install icedtea–7-plugin openjdk–7-jre -y
+
+#Extra applets
+sudo apt-get install diodon diodon-plugins  -y
+
+#Calendar Indicator
+sudo apt-get install calendar-indicator -y
+
+#Weather Indicator
+sudo apt-get install my-weather-indicator -y
+
+#Nifty System Load Indicator
+sudo apt-get install indicator-multiload -y
+
+#Fix overheating and extend battery life
+sudo apt-get install tlp tlp-rdw -y
+sudo tlp start
+
+#Install Compiz settings manager
+sudo apt-get install -y
+sudo apt-get install compizconfig-settings-manager -y
+
+#Disable the boneheaded overlay scrollbars
+gsettings set com.canonical.desktop.interface scrollbar-mode normal
+
+#Put the username back on the the panel
+gsettings set com.canonical.indicator.session show-real-name-on-panel true
+
+#Autoremove
+sudo apt-get autoremove -y
+
+#Autoclean
+sudo apt-get autoclean -y
