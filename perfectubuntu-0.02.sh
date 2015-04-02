38421eae16c00e5a791a501feccab75f7edfc304
jfreitas2015 <jeanrfreitass@gmail.com>
2015-04-02 16:58:41 -0300

    Basic improvements
    I Removed some unnecessaries packages.

--- perfectubuntu-0.02.sh
+++ perfectubuntu-0.02.sh
@@ -1,19 +1,13 @@
 #Adding Applications PPAs
-echo Adding Aplications PPAs ...
 sudo sh -c ‘echo “deb http://archive.canonical.com/ quantal partner” >> /etc/apt/sources.list’
 sudo add-apt-repository ppa:atareao/atareao -y
-sudo add-apt-repository ppa:atareao/atareao -y
-sudo add-apt-repository ppa:linrunner/tlp -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo sh -c "echo 'deb http://repository.spotify.com stable non-free' >> /etc/apt/sources.list"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
-sudo add-apt-repository ppa:foobnix-player/foobnix -y
 sudo add-apt-repository ppa:ricotz/docky -y
 sudo add-apt-repository ppa:audacity-team/daily -y
-sudo add-apt-repository ppa:jonoomph/openshot-edge -y
 sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
-echo Adding Aplications PPAs [OK]
 
 #Updating and upgrading packages
 sudo apt-get update && sudo apt-get upgrade -y
@@ -21,14 +15,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 #Deja-dup backup tool
 sudo apt-get install deja-dup -y
 
-#Terminal Notifications
-sudo apt-get install undistract-me -y
-
 #Y PPA Manager
 sudo apt-get install y-ppa-manager -y
-
-#Vuze torrent client
-sudo apt-get install vuze -y
 
 #Google Chrome
 sudo apt-get install google-chrome-stable -y
@@ -45,9 +33,6 @@ sudo apt-get install openshot openshot-doc -y
 #Install some Codecs and Enable DVD Playback
 sudo apt-get install gstreamer0.10-plugins-ugly libxine1-ffmpeg gxine mencoder libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321 libavcodec-extra -y
 sudo /usr/share/doc/libdvdread4/install-css.sh
-
-#Foobnix Audio Player
-sudo apt-get update && sudo apt-get install foobnix -y
 
 #Spotify
 sudo apt-get install spotify-client -y
@@ -87,17 +72,6 @@ sudo apt-get install my-weather-indicator -y
 
 #Nifty System Load Indicator
 sudo apt-get install indicator-multiload -y
-
-#Fix overheating and extend battery life
-sudo apt-get install tlp tlp-rdw -y
-sudo tlp start
-
-#Install Compiz settings manager
-sudo apt-get install -y
-sudo apt-get install compizconfig-settings-manager -y
-
-#Disable the boneheaded overlay scrollbars
-gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
 #Put the username back on the the panel
 gsettings set com.canonical.indicator.session show-real-name-on-panel true
