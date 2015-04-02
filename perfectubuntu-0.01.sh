ee3262e93f68ce3597e217c128c18e42df3904cd
jfreitas2015 <jeanrfreitass@gmail.com>
2015-04-02 15:26:08 -0300

    Testando
    Esse Carai

--- perfectubuntu-0.01.sh
+++ perfectubuntu-0.01.sh
@@ -1,3 +1,4 @@
+#Adding Applications PPA's TESTANDO ESSE CARAI
 echo Adding Aplications PPAs ...
 sudo sh -c ‘echo “deb http://archive.canonical.com/ quantal partner” >> /etc/apt/sources.list’
 sudo add-apt-repository ppa:atareao/atareao -y
